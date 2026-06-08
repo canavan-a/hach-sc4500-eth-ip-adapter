d8a928c9f00b98c95e23e59993fd3c075dce3edd
canavan-a <aidan.canavan3@gmail.com>
2026-06-08 12:42:59 -0400

    bug fixes

--- config.h
+++ config.h
@@ -7,5 +7,5 @@ namespace config {
     inline uint16_t    port      = 0xAF12;
     inline int         pollMs    = 5000;
     inline std::string recTopic = "hach/sc4500/rec";
-    inline std::string controlTopic = "hach/sc4500/control"
+    inline std::string controlTopic = "hach/sc4500/control";
 }

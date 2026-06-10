c8607e8940babcfeed9b401b8d0cbb81cb5e1f94
canavan-a <aidan.canavan3@gmail.com>
2026-06-10 15:13:15 -0400

    updated interval to 60s

--- config.h
+++ config.h
@@ -5,7 +5,7 @@
 namespace config {
     inline std::string ipAddress = "192.168.33.40";
     inline uint16_t    port      = 0xAF12;
-    inline int         pollMs    = 5000;
+    inline int         pollMs    = 60000;
     inline std::string recTopic = "hach/sc4500/rec";
     inline std::string controlTopic = "hach/sc4500/control";
 }
